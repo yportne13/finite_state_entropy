a5ecba52bba106f197ffd432a598d257a84696f4
yportne <yportne@163.com>
2021-11-21 08:47:26 +0000

    feat(fse_lut):generate the table base on data

--- fse_lut.py
+++ fse_lut.py
@@ -1,8 +1,9 @@
 
 class table:
-    def __init__(self,total):
-        self.total = total
+    def __init__(self,ls):
+        self.ls = ls
         self.dict = {}
+        self.table = {}
 
     def genWithData(self,data):
         dataset = set(data)
@@ -13,8 +14,31 @@ class table:
             self.dict.update({item:(data.count(item),cdf)})
             cdf = cdf + data.count(item)
         print(self.dict)
+        self.gentable()
+        for item in self.table:
+            print(item,self.table[item])
+
+    def shortState(self,toOutput,ls,item):
+        (div,mod) = divmod(ls,self.dict[item][0])
+        nextstate = self.ls*div + mod + self.dict[item][1]
+        if nextstate > 2*self.ls - 1:
+            toOutput.append(ls&0x01)
+            return self.shortState(toOutput,ls>>1,item)
+        else:
+            return nextstate
+
+
+    def gentable(self):
+        for item in self.dict:
+            table = []
+            for l in range(self.ls):
+                toOutput = []
+                ls = l + self.ls
+                state = self.shortState(toOutput,ls,item)
+                table.append((toOutput,state))
+            self.table.update({item:table})
 
 data = "ybbybb010"
 dataList = list(data)
-t = table(8)
+t = table(10)
 t.genWithData(dataList)
